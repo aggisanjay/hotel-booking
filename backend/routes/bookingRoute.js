ef0bc39c8e579f581b314f9c3a1bf3c1f86acb48
sanjay aggi <126645784+aggisanjay@users.noreply.github.com>
2025-05-20 20:54:12 +0530

    Update bookingRoute.js

--- backend/routes/bookingRoute.js
+++ backend/routes/bookingRoute.js
@@ -7,7 +7,7 @@ const bookingRouter=express.Router()
 bookingRouter.post('/check-availability',checkAvailabilityAPI)
 bookingRouter.post('/book',protect,createBooking);
 bookingRouter.get('/user',protect,getUserBookings);
-bookingRouter.post('/hotel',protect,getHotelBookings);
+bookingRouter.get('/hotel',protect,getHotelBookings);
 
 
 export default bookingRouter;
